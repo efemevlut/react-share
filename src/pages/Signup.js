6dfbf2c7eac0c3bbfa0f55aa012f8e9296648ce4
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-11-24 22:47:03 +0300

    Add formik

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,35 +1,73 @@
 import React from "react";
 import { Button, TextField, Grid, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useFormik } from "formik";
+
+const styles = makeStyles({
+  wrapper: {
+    marginTop: "5rem",
+  },
+});
 
 function Signup() {
+  const formik = useFormik({
+    initialValues: {
+      displayName: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  const signupStyles = styles();
+
   return (
-    <Container maxWidth="sm">
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            id="outlined-basic"
-            label="Display Name"
-            variant="outlined"
-            fullWidth
-          />
+    <Container maxWidth="sm" className={signupStyles.wrapper}>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              name="displayName"
+              label="Display Name"
+              variant="outlined"
+              fullWidth
+              value={formik.values.displayName}
+              onChange={formik.handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              name="email"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              name="password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary" fullWidth type="submit">
+              Submit
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary" fullWidth>
+              SignUp with Google
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth/>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth/>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary" fullWidth>
-            Submit
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary" fullWidth>
-            SignUp with Google
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </Container>
   );
 }
