3fcc50bf5fe4a24f1dd2ba942faa020a566dd75f
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-11-28 19:36:16 +0300

    Add API call user

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
+import axios from 'axios'
+
+
 const stylesFunc = makeStyles((theme) => ({
   wrapper: {
     marginTop: "10rem",
@@ -14,11 +17,30 @@ const stylesFunc = makeStyles((theme) => ({
 }));
 
 function Main() {
+
+    const [userList, setUserList] = useState()
+    const {REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN} = process.env;
+
+    const fetchData = async () => {
+        const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
+            headers: {
+                "app-id": REACT_APP_API_TOKEN,
+            }
+        })
+        setUserList(response?.data?.data)
+    }
+
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
   const mainStyles = stylesFunc();
 
   return (
     <Container className={mainStyles.wrapper} maxWidth="sm">
-      MAIN PAGE
+      {userList?.map((user) => {
+          return (<p>{`${user.title} ${user.firstName} ${user.lastName}`}</p>)
+      })}
     </Container>
   );
 }
