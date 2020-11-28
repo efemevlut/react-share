e4cd2a6ea967d42a2d9e603c90c9c2f6f17c4a53
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-11-28 19:56:08 +0300

    Add media card component

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
-import axios from 'axios'
-
+import axios from "axios";
+import MediaCard from "../components/MediaCard";
 
 const stylesFunc = makeStyles((theme) => ({
   wrapper: {
@@ -17,29 +17,34 @@ const stylesFunc = makeStyles((theme) => ({
 }));
 
 function Main() {
-
-    const [userList, setUserList] = useState()
-    const {REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN} = process.env;
-
-    const fetchData = async () => {
-        const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
-            headers: {
-                "app-id": REACT_APP_API_TOKEN,
-            }
-        })
-        setUserList(response?.data?.data)
-    }
-
-    useEffect(()=>{
-        fetchData();
-    }, [])
-
+  const [userList, setUserList] = useState();
   const mainStyles = stylesFunc();
+  const { REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN } = process.env;
+
+  const fetchData = async () => {
+    const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
+      headers: {
+        "app-id": REACT_APP_API_TOKEN,
+      },
+    });
+    setUserList(response?.data?.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Container className={mainStyles.wrapper} maxWidth="sm">
       {userList?.map((user) => {
-          return (<p>{`${user.title} ${user.firstName} ${user.lastName}`}</p>)
+        return (
+          <MediaCard
+            key={user?.id}
+            userImage={user?.picture}
+            userName={`${user?.title} ${user?.firstName} ${user?.lastName}`}
+            userEmail={user?.email}
+          />
+        );
       })}
     </Container>
   );
